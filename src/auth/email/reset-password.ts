fd749ff9c8552ce3b56451e5baa079ea9f01484f
Krish Magar <80267058+krishmagar@users.noreply.github.com>
2023-04-28 23:01:23 +0545

    Update reset-password.ts

--- src/auth/email/reset-password.ts
+++ src/auth/email/reset-password.ts
@@ -57,7 +57,7 @@ const emailTemplate = `
                             </tr>
                             <tr>
                                 <td style="text-align:center;">
-                                    <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>www.realstate.com</strong></p>
+                                    <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong></strong></p>
                                 </td>
                             </tr>
                             <tr>
