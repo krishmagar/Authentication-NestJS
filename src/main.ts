b77b8050315144327711d1efc54dac54720f01bb
Krish Magar <krishmagar010@gmail.com>
2023-06-12 07:04:10 +0545

    Added Swagger Header Authentication

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Authentication')
+    .setTitle('Complete Authentication')
     .setDescription('The cats API description')
     .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        name: 'Authorization',
+        bearerFormat: 'JWT',
+        description: 'Enter your JWT token',
+        in: 'header',
+      },
+      'jwt',
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
