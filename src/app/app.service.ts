5a23dd28c4d6eab44e2dd98583670a17034d36f9
root <krishmagar010@gmail.com>
2023-10-31 06:35:32 +0000

    .

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable, UseGuards } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return { message: 'Hello!' };
+    return { message: 'Hello World!' };
   }
 }
